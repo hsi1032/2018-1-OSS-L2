4521289c691c76d0a884dcc5980b890f4cfb2d26
SangWook <9967han@naver.com>
2018-06-05 22:56:18 +0900

    Update test_maths.py

---
+++ tests/test_maths.py
@@ -0,0 +1,16 @@
+from algorithms.maths import (
+    permutation,
+    combination
+)
+
+import unittest
+
+class TestSuite(unittest.Testcase):
+    def test_combination(self):
+        self.assertEqual(35, combination(7,3)) 
+    def test_permutation(self):
+        self.assertEqual(210, permutation(7,3))
+
+if __name__ == "__main__":
+    unittest.main()
+    
