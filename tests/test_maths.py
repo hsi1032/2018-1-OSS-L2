c0dc9fb3dc18afe5704f7873772808de7ff9ba35
Han SangWook <9967han@naver.com>
2018-06-06 00:34:59 +0900

    Update test_maths.py

--- tests/test_maths.py
+++ tests/test_maths.py
@@ -1,6 +1,8 @@
 from algorithms.maths import (
+    combination,
+    fibonacci,
     permutation,
-    combination
+    
 )
 
 import unittest
@@ -8,9 +10,11 @@ import unittest
 class TestSuite(unittest, Testcase):
   def test_combination(self):
     self.assertEqual(35, combination(7,3))
+  def test_fibonacci(self):
+    self.fibonacci(55, fibonacci(10))
   def test_permutation(self):
     self.assertEqual(210, permutation(7,3))
-  
+ 
 if __name__ == "__main__":
   unittest.main()
   
